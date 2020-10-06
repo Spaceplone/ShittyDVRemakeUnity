41575f667185522022da55d3c2b8aad1541beb3d
SpaceploneConnie <airplane-hugs@yandex.com>
2020-10-06 20:34:34 +0200

    Initial commit

---
+++ Assets/playerInput.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerInput : MonoBehaviour
+{
+    public thrusterController thrusterFrontRightStar;
+    public thrusterController thrusterFrontRightRetro;
+    public thrusterController thrusterFrontLeftRetro;
+    public thrusterController thrusterFrontLeftPort;
+    public thrusterController thrusterRearLeftPort;
+    public thrusterController thrusterRearLeftPro;
+    public thrusterController thrusterRearRightPro;
+    public thrusterController thrusterRearRightStar;
+
+    public float RightTriggerPressed;
+    public float LeftTriggerPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float frontRightStarThrust = 0.0f;
+        float frontRightRetroThrust = 0.0f;
+        float frontLeftRetroThrust = 0.0f;
+        float frontLeftPortThrust = 0.0f;
+        float rearLeftPortThrust = 0.0f;
+        float rearLeftProThrust = 0.0f;
+        float rearRightProThrust = 0.0f;
+        float rearRightStarThrust = 0.0f;
+
+        var gamepad = Gamepad.current;
+        var rightTriggerPressed = gamepad.rightTrigger.ReadValue();
+        this.RightTriggerPressed = rightTriggerPressed;
+
+        rearRightStarThrust += rightTriggerPressed;
+        frontLeftPortThrust += rightTriggerPressed;
+        //this.thrusterRearRightStar.ApplyForce(rightTriggerPressed);
+
+        //this.thrusterFrontLeftPort.ApplyForce(rightTriggerPressed);
+
+        var leftTriggerPressed = gamepad.leftTrigger.ReadValue();
+        this.LeftTriggerPressed = leftTriggerPressed;
+        frontRightStarThrust += leftTriggerPressed;
+        rearLeftPortThrust += leftTriggerPressed;
+
+        var stickState = gamepad.rightStick.ReadValue();
+
+        if (stickState.y >= 0)
+        {
+            rearLeftProThrust += stickState.y;
+            rearRightProThrust += stickState.y;
+        }
+        if (stickState.y <= 0)
+        {
+            frontLeftRetroThrust -= stickState.y;
+            frontRightRetroThrust -= stickState.y;
+
+        }
+        if (stickState.x >= 0)
+        {
+            frontLeftPortThrust += stickState.x;
+            rearLeftPortThrust += stickState.x;
+        }
+
+        if (stickState.x <= 0)
+        {
+            frontRightStarThrust -= stickState.x;
+            rearRightStarThrust -= stickState.x;
+        }
+        thrusterFrontRightStar.ApplyForce(frontRightStarThrust);
+        thrusterFrontRightRetro.ApplyForce(frontRightRetroThrust);
+        thrusterFrontLeftRetro.ApplyForce(frontLeftRetroThrust);
+        thrusterFrontLeftPort.ApplyForce(frontLeftPortThrust);
+        thrusterRearLeftPort.ApplyForce(rearLeftPortThrust);
+        thrusterRearLeftPro.ApplyForce(rearLeftProThrust);
+        thrusterRearRightPro.ApplyForce(rearRightProThrust);
+        thrusterRearRightStar.ApplyForce(rearRightStarThrust);
+    }
+}
---
+++ Assets/thrusterController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thrusterController : MonoBehaviour
+{
+    public ConstantForce2D constantForce2D;
+    public Rigidbody2D rigidbody2D;
+    public ParticleSystem particleSystem;
+    public bool invertThrust = false;
+    public bool enabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (null != this.rigidbody2D)
+        //{
+        //    this.rigidbody2D.SetRotation(this.transform.rotation.eulerAngles.y);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ApplyForce(float thrustValue )
+    {
+        if (!enabled)
+        {
+            return;
+        }
+        if (thrustValue>1)
+        {
+            thrustValue = 1;
+        }
+        var force = new Vector2(
+            0.0f, 0.0f);
+        force.y = thrustValue;
+        this.constantForce2D.relativeForce = force;
+
+        // set particles
+        if (force.x < 0.10f)
+        {
+            //this.particleSystem.Stop();
+        }
+        if (thrustValue> 0.010f)
+        {
+            //this.particleSystem.Play();
+            ParticleSystem.MainModule particleMain = this.particleSystem.main;
+            particleMain.startSpeed = thrustValue;
+            particleMain.startLifetime = 0.1f;
+        }
+
+    }
+}
